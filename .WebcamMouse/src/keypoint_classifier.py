b162b070af928b9bbca3d6522f50b93a2e02fb1b
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-04-29 16:45:57 -0500

    feat: add keypoint classifier
    Uses the model to predict the gesture being performed through the calculated landmarks.

---
+++ .WebcamMouse/src/keypoint_classifier.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import tensorflow as tf
+from src.singleton_meta import Singleton
+
+class KeyPointClassifier(object, metaclass=Singleton):
+    def __init__(
+        self,
+        model_path = 'src/utils/models/keypoint_classifier.keras', 
+    ):
+        self.model = tf.keras.models.load_model(model_path)
+        
+    def predict_gesture(self, landmarks_list):
+        result = self.model.predict(np.array([landmarks_list], dtype=np.float32))
+        result_index = np.argmax(np.squeeze(result))
+        return result_index

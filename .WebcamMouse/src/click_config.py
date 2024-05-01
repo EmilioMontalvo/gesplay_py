6f59f66cc8b40870074e7e398712035832c7258e
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-05-01 18:48:14 -0500

    feat: add click config class

---
+++ .WebcamMouse/src/click_config.py
@@ -0,0 +1,19 @@
+import json
+from src.singleton_meta import Singleton
+
+class ClickConfig(metaclass=Singleton):
+    def __init__(self):
+        self.camera_id = 0
+        self.gesture_index = 0
+    
+    def start(self):
+        pass
+
+    def load_from_json(self, json_string):
+        data = json.loads(json_string)
+        self.camera_id = data["camera_id"]
+        self.gesture_index = data["gesture_index"]
+    
+    def load_from_dict(self, data):
+        self.camera_id = data["camera_id"]
+        self.gesture_index = data["gesture_index"]

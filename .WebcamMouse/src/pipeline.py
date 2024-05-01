14ef693a1d8eb751a98b9a2b0e1a84fb56e29018
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-05-01 18:21:29 -0500

    feat: add Click Controller in pipeline

--- .WebcamMouse/src/pipeline.py
+++ .WebcamMouse/src/pipeline.py
@@ -4,6 +4,7 @@ from src.camera_manager import CameraManager
 from src.detectors.hand_detector import HandDetector
 from src.udp.udp_client import UdpClient
 from src.controllers.mouse_controller import MouseController
+from src.controllers.click_controller import ClickController
 
 
 class Pipeline:
@@ -11,9 +12,12 @@ class Pipeline:
         logging.info("Init Pipeline")
 
     def pipeline_tick(self) -> None:
+        frame=CameraManager().frame
         frame_rgb = CameraManager().get_frame_rgb()
         hand_position = HandDetector().get_hand_position(frame_rgb)
+        results_image = HandDetector().hands.process(frame_rgb)
         MouseController().act(track_loc=hand_position)
+        ClickController().act(image_results=results_image, image=frame)
 
         send_frame=CameraManager().frame
         utils.draw_circle(send_frame, hand_position)

d567eceaa99616187c1535cdafcce5e8c11198aa
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-05-08 23:13:40 -0500

    refactor: load model and predict arguments

--- .WebcamMouse/src/keypoint_classifier.py
+++ .WebcamMouse/src/keypoint_classifier.py
@@ -8,9 +8,9 @@ class KeyPointClassifier(metaclass=Singleton):
         self,
         model_path = 'src/utils/models/keypoint_classifier.keras', 
     ):
-        self.model = tf.keras.models.load_model(model_path)
+        self.model = tf.keras.models.load_model(filepath=model_path,compile=True)
         
     def predict_gesture(self, landmarks_list):
-        result = self.model.predict(np.array([landmarks_list], dtype=np.float32))
+        result = self.model.predict(np.array([landmarks_list], dtype=np.float32), verbose=0)
         result_index = np.argmax(np.squeeze(result))
         return result_index

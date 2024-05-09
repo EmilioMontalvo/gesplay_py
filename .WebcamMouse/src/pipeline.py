03f9ac8d58027433a9dfa28c3fa550066ebdd9a2
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-05-08 20:52:14 -0500

    refactor: call get_flipped_image method

--- .WebcamMouse/src/pipeline.py
+++ .WebcamMouse/src/pipeline.py
@@ -12,17 +12,19 @@ class Pipeline:
         logging.info("Init Pipeline")
 
     def pipeline_tick(self) -> None:
-        frame=CameraManager().frame
         frame_rgb = CameraManager().get_frame_rgb()
+        frame = CameraManager().get_fliped_frame()
         hand_position = HandDetector().get_hand_position(frame_rgb)
+        frame_rgb.flags.writeable = False
         results_image = HandDetector().hands.process(frame_rgb)
+        frame_rgb.flags.writeable = True
         MouseController().act(track_loc=hand_position)
         ClickController().act(image_results=results_image, image=frame)
 
         send_frame=CameraManager().frame
         utils.draw_circle(send_frame, hand_position)
         send_frame=utils.invert_frame(send_frame)
-        UdpClient().send_bytes(send_frame)
+        # UdpClient().send_bytes(send_frame)
     
 
        

e0be23ee94e5f64cf4a502e5f812e10459f701b0
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-05-09 23:41:57 -0500

    feat: add independent models for each gesture

--- .WebcamMouse/src/keypoint_classifier.py
+++ .WebcamMouse/src/keypoint_classifier.py
@@ -2,15 +2,20 @@ import os
 import numpy as np
 import tensorflow as tf
 from src.singleton_meta import Singleton
+from src.click_config import ClickConfig
 
 class KeyPointClassifier(metaclass=Singleton):
     def __init__(
         self,
-        model_path = 'src/utils/models/keypoint_classifier.keras', 
+        models_paths = ['src/utils/models/keypoint_classifier_catch.keras','src/utils/models/keypoint_classifier_nip.keras'] 
     ):
-        self.model = tf.keras.models.load_model(filepath=model_path,compile=True)
+        self.model = tf.keras.models.load_model(filepath=models_paths[ClickConfig().gesture_index],compile=True)
         
     def predict_gesture(self, landmarks_list):
         result = self.model.predict(np.array([landmarks_list], dtype=np.float32), verbose=0)
         result_index = np.argmax(np.squeeze(result))
         return result_index
+    
+    def change_model(self, gesture_index):
+        self.model = tf.keras.models.load_model(filepath=models_paths[gesture_index],compile=True)
+        return True

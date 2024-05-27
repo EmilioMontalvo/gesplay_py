526c2a868cdbcbd6adc5079041730b924870a337
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-05-26 21:16:21 -0500

    feat: cursor game proptotype

--- scripts/cursor/shovel.gd
+++ scripts/cursor/shovel.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
-
+@onready var audioPlayer=$AudioStreamPlayer2D
 @onready var destructionPoligon=$Destruccion/CollisionPolygon2D
+@onready var animation=$AnimationPlayer
+@onready var grab_timer = Timer.new()
+
 var grabbed = false
+var firstTime=true
 signal touched
 
 
 func _ready():
-	pass
+	add_child(grab_timer)
+	grab_timer.wait_time = 3.0
+	grab_timer.one_shot = true
+	#TODO: get audio from global config
+	audioPlayer.volume_db=0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,11 +30,15 @@ func _on_collision_detection_area_entered(area:Area2D):
 
 func _on_area_2d_mouse_entered():
 	grabbed=true
+	if firstTime:
+		audioPlayer.play()
+		animation.stop()
+		firstTime=false
 
 
 func _on_collision_detection_body_entered(body):
 	emit_signal("touched",body)
-	
+
 func get_destruction_polygon():
 	return destructionPoligon
 

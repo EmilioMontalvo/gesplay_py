7d86da6b3e14434f1f0486b462b1aed426020a26
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-05-30 11:18:24 -0500

    feat: cursor configuration trail

---
+++ scripts/cursor/line_trail.gd
@@ -0,0 +1,23 @@
+extends Line2D
+
+@export var drawTrail=false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if drawTrail:
+		add_point(get_global_mouse_position())
+		if points.size()>10:
+			remove_point(0)
+
+
+func _on_panel_mouse_entered():
+	drawTrail=true
+
+
+func _on_panel_mouse_exited():
+	drawTrail=false
+	points=[]

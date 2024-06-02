4f10451bc1026c87ba4e6991450fcef81fbdb40e
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-02 12:01:21 -0500

    feat: help button added to slider field

--- scripts/cursor/slider_field.gd
+++ scripts/cursor/slider_field.gd
@@ -7,7 +7,7 @@ signal value_changed
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spinBox.update_on_text_changed = true
-
+	spinBox.value=slider.value
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,9 +16,9 @@ func _process(delta):
 
 func _on_h_slider_3_value_changed(value):
 	spinBox.value=value
-	value_changed.emit()
+	value_changed.emit(value)
 
 
 func _on_spin_box_value_changed(value):
 	slider.value=value
-	value_changed.emit()
+	value_changed.emit(value)

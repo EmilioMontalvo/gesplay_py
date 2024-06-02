cab18ee1c68a7cf62d7151f23737119403fa1ab3
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-02 12:16:24 -0500

    feat: hover an pressed slyles for help button

---
+++ scripts/menu/help_button.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal help_request
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	help_request.emit()

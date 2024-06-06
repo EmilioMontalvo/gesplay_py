4d0c2afbb77ab94a33a7174348662d26f95a52a5
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-06 17:49:58 -0500

    fix: draggable panel wrong offset

--- scripts/enhanced_camera.gd
+++ scripts/enhanced_camera.gd
@@ -1,8 +1,8 @@
 extends Control
 
 var paused=false
-var dragPoint = Vector2()
-var following=false
+var dragPointLocal = Vector2()
+var followingLocal=false
 var window=true
 
 
@@ -19,11 +19,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if following and visible and window and DisplayServer.get_window_list().has(window_id):
+	if followingLocal and visible and window and DisplayServer.get_window_list().has(window_id):
+		print("window",DisplayServer.window_get_position(window_id))
+		print("mouse",get_global_mouse_position())
+		print("local mouse",dragPointLocal)
 		DisplayServer.window_set_position(Vector2(
-			DisplayServer.window_get_position(window_id)) + get_global_mouse_position() - dragPoint,
+			DisplayServer.window_get_position(window_id)) + get_global_mouse_position() - dragPointLocal,
 			window_id)
-		print(window_id,DisplayServer.window_get_position(window_id))
+		
 
 func _on_go_back_pressed():
 	go_back.emit()
@@ -38,13 +41,11 @@ func _on_pause_pressed():
 func _on_close_pressed():
 	$ConfirmationDialog.visible=true
 
-
-func _on_drag_panel_gui_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index==MOUSE_BUTTON_LEFT:
-			following=not following
-			dragPoint=get_local_mouse_position()
-
 func set_window_id(id):
 	window_id=id
 			
+
+
+func _on_drag_panel_dragged_panel(following,dragPoint):
+	followingLocal=following
+	dragPointLocal=dragPoint * scale
---
+++ scripts/menu/drag_panel.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var following=false
+var dragPoint = Vector2()
+
+signal dragged_panel
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index==MOUSE_BUTTON_LEFT:
+			print(event)
+			following=not following
+			dragPoint=get_local_mouse_position()
+			dragged_panel.emit(following,dragPoint)

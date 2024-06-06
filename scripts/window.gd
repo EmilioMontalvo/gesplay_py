02edfff8698b33b9f17e1feb4605d039554df128
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-05 20:44:34 -0500

    fix: black background

--- scripts/window.gd
+++ scripts/window.gd
@@ -3,7 +3,8 @@ extends Window
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 1)
+	# DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 1)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

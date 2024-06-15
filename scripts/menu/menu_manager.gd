c5fdff674ba37fe80e3ab4a29eea7b081a629952
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-14 21:48:18 -0500

    feat: load packed scene in menu manager

--- scripts/menu/menu_manager.gd
+++ scripts/menu/menu_manager.gd
@@ -63,9 +63,12 @@ func load_instace_menu(instance:Node):
 	root_scene.remove_child(root_scene.get_children()[last_child_index-1])
 	root_scene.add_child(instance)
 	base_instance=instance
-	
 
-	
+func load_packed_scene(scene:PackedScene):
+	if base_instance:
+		get_tree().root.remove_child(base_instance)
+		base_instance=null
+	get_tree().change_scene_to_packed(scene)
 	
 func set_current_scene(node):
 	current_scene=node

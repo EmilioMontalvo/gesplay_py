026c366e4b5a02523347cdd27a4682436fab844a
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-17 22:46:47 -0500

    feat: destroy window method

--- .WebcamMouse/src/windows/cursor_window.py
+++ .WebcamMouse/src/windows/cursor_window.py
@@ -8,45 +8,47 @@ class CursorWindow(metaclass=Singleton):
 
     def __init__(self):
         self.ancho_pantalla,  self.alto_pantalla = pyautogui.size()
+        self.root = tk.Tk()
 
-    def run(self):
-        
-
+    def start(self):
         def update_mouse_position():
             x, y = pyautogui.position()
             
             pointxy = (x+50, y+50)
             canvas.coords(cimg, pointxy)
-            root.after(10, update_mouse_position)
+            self.root.after(10, update_mouse_position)
 
-        root = tk.Tk()
-        root.attributes("-topmost", True)  
-        root.attributes("-transparentcolor", root['bg'])
+        self.root.attributes("-topmost", True)  
+        self.root.attributes("-transparentcolor", self.root['bg'])
         #root.config(cursor="none")
         
         # Obtener el tamaño de la pantalla con PyAutoGUI
         ancho_pantalla, alto_pantalla = pyautogui.size()
 
         # Establecer el tamaño de la ventana
-        root.after(0, root.geometry, f"{self.ancho_pantalla}x{self.alto_pantalla}")
-        root.geometry(f"{self.ancho_pantalla}x{self.alto_pantalla}")
+        self.root.after(0, self.root.geometry, f"{self.ancho_pantalla}x{self.alto_pantalla}")
+        self.root.geometry(f"{self.ancho_pantalla}x{self.alto_pantalla}")
 
         # Cargar la imagen sin aplicar transparencia
         img_pil = Image.open('cursor_1.png')
         img_tk = ImageTk.PhotoImage(img_pil)
 
-        canvas = tk.Canvas(root)
+        canvas = tk.Canvas(self.root)
         canvas.pack(expand=True, fill=tk.BOTH)  # Expandir el Canvas con la ventana
 
         cimg = canvas.create_image(200, 100, image=img_tk)
 
         # Iniciar la actualización continua de la posición del mouse
         update_mouse_position()
-        root.update_idletasks()
-        root.overrideredirect(True)
+        self.root.update_idletasks()
+        self.root.overrideredirect(True)
     
         # Obtener el handle de la ventana
-        root.mainloop()
+        self.root.mainloop()
+    
+    def destory(self):
+        self.root.quit()  # Salir del bucle principal de tkinter
+        self.root.destroy()
 
 if __name__ == '__main__':
     CursorWindow().run()

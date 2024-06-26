66f32590dab8556e063efba798ee1d9cb5944eff
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-06-26 10:59:07 -0500

    feat: balloon minigame mecanic

--- scripts/cursor/config/balloon_minigame.gd
+++ scripts/cursor/config/balloon_minigame.gd
@@ -1,16 +1,35 @@
 extends Node2D
 
 var balloon_scene = preload("res://scenes/menu/components/cursor/balloon.tscn")
+@export var spawn_interval: float = 1.0
+@export var max_balloons: int = 4
+
+# Internal variables
+var balloon_count: int = 0
 
 func _ready():
-	create_balloon()
+	# Start the timer
+	var timer = Timer.new()
+	timer.wait_time = spawn_interval
+	timer.one_shot = false
+	timer.connect("timeout", _on_Timer_timeout)
+	add_child(timer)
+	timer.start()
+	
+	# Initial balloon creation
+	for i in range(max_balloons):
+		create_balloon()
+
+func _on_Timer_timeout():
+	if balloon_count < max_balloons:
+		create_balloon()
 
 func create_balloon():
 	var balloon_instance = balloon_scene.instantiate()
 	
 	# Set a random position for the balloon
-	var random_x = randi_range(0, get_viewport().size.x)
-	var random_y = randi_range(0, get_viewport().size.y)
+	var random_x = randi_range(0, get_viewport().size.x-30)
+	var random_y = randi_range(0, get_viewport().size.y-30)
 	balloon_instance.position = Vector2(random_x, random_y)
 	
 	# Connect the 'popped' signal to the '_on_balloon_popped' method
@@ -18,8 +37,11 @@ func create_balloon():
 	
 	# Add the balloon instance to the scene
 	add_child(balloon_instance)
+	
+	balloon_count += 1
 
 func _on_balloon_popped():
+	balloon_count -= 1
 	$CanvasLayer/Control.add_one()
 
 

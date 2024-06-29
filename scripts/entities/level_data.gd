8867f2e7c8bddcd59c94f6dda819b651962a8a72
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-06-29 09:22:58 -0500

    feat: add level data entity

---
+++ scripts/entities/level_data.gd
@@ -0,0 +1,11 @@
+extends RefCounted
+class_name LevelData
+
+var profile_id: String
+var level_id: String
+var mistake: int
+var elapsed_time: float
+var score: int
+var stars: int
+var completed: bool
+var date_time: String

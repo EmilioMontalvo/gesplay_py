227843ed38a01c3b806e03a28d7c1a2323b3b1d1
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2024-07-03 20:07:01 -0500

    feat: confirmation dialog in configuration scenes

--- scripts/menu/navigation.gd
+++ scripts/menu/navigation.gd
@@ -2,10 +2,19 @@ extends Control
 
 @export var scene:int = 1
 @export var locked=false
+@export var confirm_dialog=false
+@export var confirm_dialog_text="Salir sin guardar"
+
+@onready var confirm_control=$ConfirmDialogWindow
+
 
 func _on_pressed():
-	if not locked:
-		MenuManager.load_menu(scene)
+	
+	if confirm_dialog:
+		confirm_control.visible=true
+	else:
+		if not locked:
+			MenuManager.load_menu(scene)
 
 
 func _on_acept_pressed():
@@ -18,3 +27,12 @@ func _on_focus_entered():
 
 func _on_focus_exited():
 	modulate=Color(1,1,1,1)
+
+
+func _on_confirm_dialog_window_acepted():
+	if not locked:
+			MenuManager.load_menu(scene)
+
+
+func _on_confirm_dialog_window_canceled():
+	confirm_control.visible=false

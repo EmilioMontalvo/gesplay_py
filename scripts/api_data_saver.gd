180e5639b16cce3950ef06134396fd5c7aa9bff5
Mateo Díaz <mateo.diaz@epn.edu.ec>
2024-07-10 03:49:28 -0500

    feat: add methods to save images and save last profile

--- scripts/api_data_saver.gd
+++ scripts/api_data_saver.gd
@@ -6,6 +6,7 @@ const DELETE_PROFILE_ENDPOINT: String = "/profiles"
 const SETTINGS_ENDPOINT: String = "/game-settings"
 const GAME_DATA_ENDPOINT: String = "/profiles/{profile_id_db}/game-data/{game}"
 const LEVEL_ENDPOINT: String = "/profiles/{profile_id_db}/game-data/{game}/level/{level}"
+const PROFILE_IMAGE_ENDPOINT: String = "/profiles/me/{profile_id_db}/image"
 
 
 
@@ -16,6 +17,24 @@ static func save_profile(profile_data: Dictionary):
 static func edit_profile(profile_data: Dictionary):
 	var endpoint = PROFILES_ENDPOINT + "/" + profile_data.get("id")
 	RequestManager.make_request(endpoint, RequestManager.get_auth_headers(), HTTPClient.METHOD_PUT, profile_data)
+	
+static func save_image_profile(image_profile: Image, profile_id: String):
+	var endpoint = PROFILE_IMAGE_ENDPOINT.format({"profile_id_db": profile_id})
+	var body: PackedByteArray
+	body.append_array("--BodyBoundaryHere".to_utf8_buffer())
+	body.append_array("\r\nContent-Disposition: form-data; name=\"file\"; filename=\"Treasure.png\"\r\n".to_utf8_buffer())
+	body.append_array("Content-Type: image/png\r\n".to_utf8_buffer())
+	body.append_array("Content-Transfer-Encoding: base64\r\n".to_utf8_buffer())
+	body.append_array("\r\n".to_utf8_buffer())
+	body.append_array(Marshalls.raw_to_base64(image_profile.save_png_to_buffer()).to_utf8_buffer())
+	body.append_array("\r\n".to_utf8_buffer())
+	body.append_array("--BodyBoundaryHere".to_utf8_buffer())
+	var headers: PackedStringArray = [
+		RequestManager.get_auth_headers()[0],
+		"Content-Length: " + str(body.size()),
+		"Content-Type: multipart/form-data; boundary=\"BodyBoundaryHere\""
+	]
+	RequestManager.make_request(endpoint, headers, HTTPClient.METHOD_PUT, body)
 
 #func load_profiles() -> Array:
 #	var endpoint = PROFILES_ENDPOINT
@@ -29,10 +48,9 @@ static func delete_profile(profile_id: String):
 	var endpoint = DELETE_PROFILE_ENDPOINT + "/" + profile_id
 	RequestManager.make_request(endpoint, RequestManager.get_auth_headers(), HTTPClient.METHOD_DELETE)
 
-#func save_last_profile_id(profile_id: String):
-#	var endpoint = LAST_PROFILE_ENDPOINT
-#	var data = {"profile_id": profile_id}
-#	RequestManager.make_request(endpoint, RequestManager.get_auth_headers(), HTTPClient.METHOD_POST, data)
+static func save_last_profile_id(profile_id: String):
+	var endpoint = LAST_PROFILE_ENDPOINT + "?profile_id=" + profile_id
+	RequestManager.make_request(endpoint, RequestManager.get_auth_headers(), HTTPClient.METHOD_PUT)
 
 #func load_last_profile_id() -> String:
 #	var endpoint = LAST_PROFILE_ENDPOINT
